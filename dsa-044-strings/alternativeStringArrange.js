2a18061ab9398f5f144b4e8ee7e894da5cf4153b
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-04-02 15:26:52 +0545

    alternative string arrange

---
+++ dsa-044-strings/alternativeStringArrange.js
@@ -0,0 +1,23 @@
+const AlternativeStringArrange=(str1,str2)=>{
+    if(typeof str1!=='string' || typeof str2!=='string'){
+        return 'Not String(S)';
+    }
+
+    let outStr='';
+
+    const firstStringLength=str1.length;
+    const secondStringLength=str2.length;
+
+    const absLength=firstStringLength>secondStringLength?firstStringLength:secondStringLength;
+
+    for(let i=0;i<absLength;i++){
+        if(i<firstStringLength){
+            outStr+=str1[i];
+        }
+
+        if(i<secondStringLength){
+            outStr+=str2[i];
+        }
+    }
+    return outStr;
+}
