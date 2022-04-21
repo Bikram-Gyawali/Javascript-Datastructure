146f9afcbed2efaae0910d9e6d0bbd092ca8b675
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-04-21 06:03:34 +0545

    result

--- dsa-044-strings/toLowerCase.js
+++ dsa-044-strings/toLowerCase.js
@@ -1,11 +1,13 @@
 
 
 const lower=(str)=>{
-    if(typeof str!="string"){
+    if(typeof str!=="string"){
         throw new TypeError("Invalid Input Type");
     }
-    return str.replace(/[A_Z]/g,(_,indexOfUpperChar)=>{
+    return str.replace(/[A-Z]/g,(_,indexOfUpperChar)=>{
         const asciiCode=str.charCodeAt(indexOfUpperChar);
         return String.fromCharCode(asciiCode + 32);
     })
 }
+
+console.log(lower("BIKRAM"));
