e089397bf8f1ff1a93d386ebb8411383cdbaf4f3
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-07-12 23:29:45 +0545

    sortings

--- sortings/stoogeSort.js
+++ sortings/stoogeSort.js
@@ -1,15 +1,20 @@
-export function stoogeSort(items, leftEnd, rightEnd) {
+/*
+ * Stooge Sort sorts an array based on divide and conquer principle
+ * note the exceptionally bad time complexity
+ *
+ */
+export function stoogeSort (items, leftEnd, rightEnd) {
   if (items[rightEnd - 1] < items[leftEnd]) {
-    const temp = items[leftEnd];
-    items[leftEnd] = items[rightEnd - 1];
-    items[rightEnd - 1] = temp;
+    const temp = items[leftEnd]
+    items[leftEnd] = items[rightEnd - 1]
+    items[rightEnd - 1] = temp
   }
-  const length = rightEnd - leftEnd;
+  const length = rightEnd - leftEnd
   if (length > 2) {
-    const third = Math.floor(length / 3);
-    stoogeSort(items, leftEnd, rightEnd - third);
-    stoogeSort(items, leftEnd + third, rightEnd);
-    stoogeSort(items, leftEnd, rightEnd - third);
+    const third = Math.floor(length / 3)
+    stoogeSort(items, leftEnd, rightEnd - third)
+    stoogeSort(items, leftEnd + third, rightEnd)
+    stoogeSort(items, leftEnd, rightEnd - third)
   }
-  return items;
+  return items
 }
