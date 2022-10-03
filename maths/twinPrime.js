3622cebf25d213851a47fd4465f1f14cb892812f
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-10-03 23:11:41 +0545

    primecheck funtion plaaced

--- maths/twinPrime.js
+++ maths/twinPrime.js
@@ -1,4 +1,18 @@
+const PrimeCheck = (n) => {
+  // input: n: int
+  // output: boolean
+  if (n === 1) return false;
+  if (n === 0) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
 
+  for (let i = 3; i * i <= n; i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 function twinPrime(n) {
   const prime = PrimeCheck(n); // function call
