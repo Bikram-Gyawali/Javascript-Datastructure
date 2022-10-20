91f77d0776f015a60b887f29007ef713ee6a7f45
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-10-20 21:43:18 +0545

    num to roman

---
+++ maths/numtoRoman.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] numbers =  { 1000, 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1};
+        String[] strs =  { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<numbers.length;i++){
+            while(num >= numbers[i]){
+                num -= numbers[i];
+                sb.append(strs[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
