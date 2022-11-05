8778778b1553a519ca0e54b5ddeaac4b8af35277
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-05 22:47:26 +0545

    some crazy js

--- maths/trickers.js
+++ maths/trickers.js
@@ -6,3 +6,15 @@ x[+[]] === x[+[]]; // false
 // reciporcal
 x === y; // true
 1/x === 1/y; // false
+
+// prototypes in js are babbal k ho k bujna garo xa
+var someone = function() {};
+someone.prototype.attributes = {};
+
+var alice = new someone();
+alice.attributes.name = 'Alice';
+
+var bob = new someone();
+bob.attributes.name = 'Bob';
+
+var eve = new someone();
