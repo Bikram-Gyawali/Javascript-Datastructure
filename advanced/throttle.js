631537802a15e4dad1de93f39a57204bece66ec6
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-02-04 21:13:44 +0545

    throttle advanced func

--- advanced/throttle.js
+++ advanced/throttle.js
@@ -1,18 +1,17 @@
 function throttle(func, delay) {
-    let wait = false;
-  
-    return (...args) => {
-      if (wait) {
-          return;
-      }
-  
-      func(...args);
-      wait = true;
-      setTimeout(() => {
-        wait = false;
-      }, delay);
+  let wait = false
+
+  return (...args) => {
+    if (wait) {
+      return
     }
+
+    func(...args)
+    wait = true
+    setTimeout(() => {
+      wait = false
+    }, delay)
   }
+}
 
-
-  
+//   the throttle function will execute a provided function func, update a wait variable to true, and then starts a timer which will reset the wait parameter after the delay is passed. If the throttle function is called again it will either call the provided function or simply return if the wait parameter is still true.
