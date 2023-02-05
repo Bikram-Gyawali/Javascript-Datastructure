310fd6d26503ffd908fcbe4e63439fe9ec733e10
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-02-05 23:51:42 +0545

    call once advanced func

--- advanced/once.js
+++ advanced/once.js
@@ -10,4 +10,4 @@ function once(func) {
   }
 
 
-  
+//   The Once function is a method that will prevent executing if already called. This is especially useful while working with event listeners, where you often encounter functions that only should run once. Instead of removing event listeners every time you can use the Once function in JavaScript.
