5b3cd51dde2eb12034adee3eec4a4c14e4f3ebce
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-02-09 23:55:29 +0545

    partial advanced func

--- advanced/pipe.js
+++ advanced/pipe.js
@@ -1,5 +1,26 @@
 function pipe(...funcs) {
-    return function piped(...args) {
-      return funcs.reduce((result, func) => [func.call(this, ...result)], args)[0];
-    };
+  return function piped(...args) {
+    return funcs.reduce((result, func) => [func.call(this, ...result)], args)[0]
   }
+}
+
+// Define the functions that add to the string
+function addPrefix(str) {
+  return 'prefix-' + str
+}
+
+function addSuffix(str) {
+  return str + '-suffix'
+}
+
+function toUppercase(str) {
+  return str.toUpperCase()
+}
+
+// Create a piped function that applies the three functions in the correct order
+const decorated1 = pipe(addPrefix, addSuffix, toUppercase)
+const decorated2 = pipe(toUppercase, addPrefix, addSuffix)
+
+// Call the piped function with the input string
+const result1 = decorated1('hello') // PREFIX-HELLO-SUFFIX
+const result2 = decorated2('hello') // prefix-HELLO-suffix
