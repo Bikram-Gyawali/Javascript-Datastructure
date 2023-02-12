f87fca0b9b701d47c36d0030a8f6744af5fb3234
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-02-12 23:13:55 +0545

    pick advanced function

--- advanced/pick.js
+++ advanced/pick.js
@@ -8,3 +8,15 @@ function pick(obj, keys) {
 }
 
 // The Pick function in JavaScript is used to select specific values from an object. It is a way to create a new object by selecting certain properties from a provided project. It is a functional programming technique that allows extracting a subset of properties from any object if the properties are available .
+
+
+const obj = {
+    id: 1,
+    name: 'Paul',
+    password: '82ada72easd7',
+    role: 'admin',
+    website: 'https://www.paulsblog.dev',
+};
+
+const selected = pick(obj, ['name', 'website']);
+console.log(selected); // { name: 'Paul', website: 'https://www.paulsblog.dev' }
