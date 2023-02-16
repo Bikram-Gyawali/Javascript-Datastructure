86c3dde3bbe96fe9ec57d9ef1ef9b410070b291f
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-02-16 21:44:41 +0545

    zip adv

--- advanced/zip.js
+++ advanced/zip.js
@@ -7,3 +7,14 @@ function zip(...arrays) {
 
 
 //   The Zip function is a JavaScript function that matches each passed array of elements to another array element and is used to combine multiple arrays into a single array of tuples. The resulting array will contain the corresponding elements from each array. Often, this functionality is used when working with data from multiple sources that need to be merged or correlated in some way./
+
+// Define the arrays that contain the coordinates
+const xCoordinates = [1, 2, 3, 4];
+const yCoordinates = [5, 6, 7, 8];
+const zCoordinates = [3, 6, 1, 7];
+
+// Create a zipped array of points
+const points = zip(xCoordinates, yCoordinates, zCoordinates);
+
+// Use the zipped array of points
+console.log(points);  // [[1, 5, 3], [2, 6, 6], [3, 7, 1], [4, 8, 7]]
