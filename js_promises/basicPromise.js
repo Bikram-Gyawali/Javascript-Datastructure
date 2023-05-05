cf9724352ad11260031af109833afd8a52904f9f
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-05-05 18:55:39 +0545

    changes and fixes

--- js_promises/basicPromise.js
+++ js_promises/basicPromise.js
@@ -3,10 +3,9 @@ const users = [
   { fullname: "jane foster", age: 20 },
 ];
 
-
 async function getUsers(users) {
   return new Promise(async (resolve, reject) => {
-    const user = users.find((user) => user.fullname === "john does");
+    const user = users.find((user) => user.fullname === "john doe");
     if (user) {
       resolve(user);
     }
@@ -16,6 +15,7 @@ async function getUsers(users) {
 
 const promise = getUsers(users);
 
+console.log("------started------");
 promise
   .then((user) => {
     console.log(user);
@@ -23,3 +23,12 @@ promise
   .catch((err) => {
     console.log(err);
   });
+console.log("------running------");
+promise
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+console.log("------stopper------");
